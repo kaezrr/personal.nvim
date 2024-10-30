1d1919d4e90fe98ffb0683658d4af6f12289f2d3
kaezrr <107052359+kaezrr@users.noreply.github.com>
2024-10-30 09:05:45 +0000

    Configure neo-tree to show hidden files

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -18,6 +18,11 @@ return {
       width = 30,
     },
     filesystem = {
+      filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',

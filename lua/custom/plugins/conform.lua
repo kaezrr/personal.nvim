deb5bb3fdbee5e043534fe6cfadeb34f34f10627
kaezrr <107052359+kaezrr@users.noreply.github.com>
2024-11-04 08:47:04 +0000

    Add formatters

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -32,11 +32,13 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      cpp = { 'clang-format' },
+      c = { 'clang-format' },
+      html = { 'prettierd' },
+      css = { 'prettierd' },
+      markdown = { 'prettierd' },
+      json = { 'prettierd' },
+      javascript = { 'prettierd' },
     },
   },
 }

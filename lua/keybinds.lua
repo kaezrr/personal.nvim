7d1eea75c32f5163fc8d9dceabac1cf61bd97322
kaezrr <107052359+kaezrr@users.noreply.github.com>
2024-11-15 15:19:43 +0000

    Add new keybinds for saving and quitting

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -29,3 +29,8 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('n', '<leader>w', ':update<CR>', { desc = 'Save file', silent = true })
+vim.keymap.set('n', '<leader>W', ':wall<CR>', { desc = 'Save all files', silent = true })
+vim.keymap.set('n', '<leader>x', ':q<CR>', { desc = 'Close file', silent = true })
+vim.keymap.set('n', '<leader>X', ':q!<CR>', { desc = 'Close file forcefully', silent = true })

e5a52d0f5adab8f869d8b26565e11a7ab24beb3b
kaezrr <107052359+kaezrr@users.noreply.github.com>
2024-12-07 23:46:15 +0530

    Update

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -207,7 +207,7 @@ return {
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua', -- Used to format Lua code
-      'prettier',
+      'prettierd',
       'clangd',
       'clang-format',
       'typescript-language-server',
